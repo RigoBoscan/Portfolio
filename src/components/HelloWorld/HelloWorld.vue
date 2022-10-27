4370c8385d4ab51f0bae8982e448c2da2c227fb7
rigobertourdaneta <105236167+RigoBoscan@users.noreply.github.com>
2022-10-27 19:44:31 -0400

    add animation

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container class="container-animation">
+    <div class="container-information pa-0">
+      <h1 class="mt-10">¡Hola!</h1>
+      <h1 class="mt-9">
+        Mi nombre <br />
+        es Victor y soy un <br />
+        <b class="b-blue">Diseñador UI</b>
+      </h1>
+      <img
+        class="image-avatar mt-3"
+        src="../../assets/img/victor-avatar.png"
+        alt=""
+      />
+    </div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+    <div class="burbuja"></div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss">
+@import "./HelloWorld.scss";
+</style>
---
+++ src/components/layouts/header/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar height="80px" flat app color="#F8FBFF" dark>
+    <div class="d-flex align-center header">
+      <h2>PORTFOLIO</h2>
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        width="100"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      width="140px"
+      height="47px"
+      depressed
+      color="#E5EFFF"
+      class="btn-contact"
+      :to="{ name: 'about' }"
+    >
+      Contáctame
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "./header.scss";
+</style>
